abfc94d0337cae8b9795e317ac47e3ecf513b04e
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-10-22 15:21:56 +0530

    use named params

--- sonetel/recording.py
+++ sonetel/recording.py
@@ -12,54 +12,49 @@ class Recording(util.Resource):
 
     def __init__(self, access_token: str = None):
         super().__init__(access_token)
-        self._url = f'{const.API_URI_BASE}{const.API_ENDPOINT_CALL_RECORDING}?account_id={self._accountid}'
+        self._url = f'{const.API_URI_BASE}{const.API_ENDPOINT_CALL_RECORDING}'
 
-    def get(self, **kwargs):
+    def get(self,
+            start_time: str = None,
+            end_time: str = None,
+            file_access_details: bool = False,
+            voice_call_details: bool = False,
+            rec_id: str = None
+            ):
         """
         Get a list of all the call recordings.
 
-        - start_time: Timestamp. The start timestamp in the format YYYYMMDDTHH:MM:SSZ. Example 20201231T23:59:59. Limit
-         the results to recordings created after this timestamp.
-        - end_time: Timestamp. The end timestamp in the format YYYYMMDDTHH:MM:SSZ. Example 20221123T18:59:59. Limit the
-         results to recordings created before this timestamp.
-        - rec_id: String. The unique recording ID. If not included, returns all the recordings.
-        - access_details: Boolean. Include the details needed to download recordings.
-        - call_details: Boolean. Include the details of the voice calls.
+        :param start_time: The start timestamp in the format YYYYMMDDTHH:MM:SSZ. Example 20201231T23:59:59. Limit the results to recordings created after this timestamp.
+        :param end_time: The end timestamp in the format YYYYMMDDTHH:MM:SSZ. Example 20221123T18:59:59. Limit the results to recordings created before this timestamp.
+        :param rec_id: The unique recording ID. If not included, returns all the recordings.
+        :param file_access_details: Boolean. Include the details needed to download recordings.
+        :param voice_call_details: Boolean. Include the details of the voice calls.
         """
 
         url = self._url
 
-        if len(kwargs) > 0:
-            params = []
+        if rec_id:
+            # Get a single recording
+            url += f'/{rec_id}'
+        else:
+            url += f'?account_id={self._accountid}'
+
+            # TODO:
+            #   1. Check start_time and end_time format
+            #   2. Check end_time > start_time
+
+            if start_time and end_time:
+                url += f'&created_date_max={end_time}&created_date_min={start_time}'
+
             fields = []
-            try:
-                if kwargs['access_details']:
-                    fields.append('file_access_details')
-            except KeyError:
-                pass
-            try:
-                if kwargs['call_details']:
-                    fields.append('voice_call_details')
-            except KeyError:
-                pass
-            try:
-                if kwargs['start_time']:
-                    params.append(f'created_date_min={kwargs["start_time"]}')
-            except KeyError:
-                pass
-            try:
-                if kwargs['end_time']:
-                    params.append(f'created_date_min={kwargs["end_time"]}')
-            except KeyError:
-                pass
 
-            # Add fields and params to url.
+            if file_access_details:
+                fields.append('file_access_details')
+            if voice_call_details:
+                fields.append('voice_call_details')
+
             if len(fields) > 0:
-                url += "fields=" + ",".join(fields)
-
-            if len(params) > 0:
-                for p in params:
-                    url += p
+                url += '&fields=' + ','.join(fields)
 
         return util.send_api_request(token=self._token, uri=url, method='get')
 
@@ -68,3 +63,4 @@ class Recording(util.Resource):
         delete call recording
         """
         raise NotImplementedError
+
