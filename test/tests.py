0dc3b864157b1164433909827e22d51d4f232ab7
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-11-11 06:21:28 +0530

    optimise

--- test/tests.py
+++ test/tests.py
@@ -12,78 +12,86 @@ pswd = os.environ.get('sonetelPassword')
 if not user or not pswd:
     raise Exception('Cannot fetch username or password from environment variables.')
 
-def TestAuth(u, p, test_count):
-    sntl = sonetel.Auth(username=u, password=p)
-    token = sntl.get_access_token()
-    test_string = token.split('.')
-    
+def test_auth( user_name, password, count):
+    """
+    test
+    """
+    sntl = sonetel.Auth(username = user_name, password=password)
+    access_token = sntl.get_access_token()
+    test_string = access_token.split('.')
+
     try:
-        print(f"Test {test_count}")
+        print(f"Test {count}")
         assert test_string[0] == 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9'
-    except AssertionError as e:
-        print(f'\tFail. {str(e)}')
+    except AssertionError as assert_error:
+        print(f'\tFail. {str(assert_error)}')
     else:
-        print(f'\tOK')
-    return token
+        print('\tOK')
+    return access_token
 
+def test_account(access_token):
+    """
+    Test the Account resource
+    """
+    a = sonetel.Account(access_token)
 
-def TestAccount(token, test_count):
+    tests = [
+        {
+            'name': 'account_id',
+            'test': a.get_accountid(),
+            'return_type': int
+        },
+        {
+            'name': 'get_balance',
+            'test': a.get_balance(),
+            'return_type': str
+        },
+        {
+            'name': 'get_balance_currency',
+            'test': a.get_balance(currency=True),
+            'return_type': str
+        },
+        {
+            'name': 'get_account',
+            'test': a.get(),
+            'return_type': dict
+        }
+    ]
+
+    for test in tests:
+        if isinstance(test['test'],test['return_type']):
+            print(f'\t\t Success {test["name"]} - {test["test"]}')
+        else:
+            print(f'\t\t Failed {test["name"]}')
+
+def test_voice_app(access_token):
+    """
+    Test the VoiceApp resource
+    """
     # To be implemented
-    a = sonetel.Account(token)
-    try:
-        assert type(a.get_accountid()) == type(123)
-    except AssertionError as e:
-        print(f'\t\t Failed {test_count}.1 get_accountid')
-    else:
-        print(f'\t\t Success {test_count}.1 get_accountid')
-    
-    try:
-        assert type(a.get_balance()) == type('string')
-    except AssertionError as e:
-        print(f'\t\t Failed {test_count}.2 get_balance')
-    else:
-        print(f'\t\t Success {test_count}.2 get_balance')
-    
-    try:
-        assert type(a.get_balance(currency=True)) == type('string')
-    except AssertionError as e:
-        print(f'\t\t Failed {test_count}.3 get_balance_currency')
-    else:
-        print(f'\t\t Success {test_count}.3 get_balance_currency')
-    
-    try:
-        assert type(a.get()) == type ({})
-    except AssertionError as e:
-        print(f'\t\t Failed {test_count}.4 get')
-    else:
-        print(f'\t\t success {test_count}.4 get')
-    
+    assert True is True
 
-def TestVoiceApps(token, test_count):
-    # To be implemented
-    assert True == True
-
-
-def RunTest(test_name,token,test_count):
-
+def run_test(test_name,access_token,test_count):
+    """
+    Run tests using the function passed
+    """
     try:
-        print(f"Test {test_count} - {test_name}")
-        test_name(token, test_count)
-    except AssertionError as e:
-        print(f'\tFail. {str(e)}')
+        print(f"Test {test_count} - {test_name.__name__}")
+        test_name(access_token)
+    except AssertionError as assert_error:
+        print(f'\tFail. {str(assert_error)}')
     else:
-        print(f'\tOK')
+        print('\tOK')
     finally:
         test_count += 1
 
     return test_count
 
-
 if __name__ == '__main__':
-    test_count = 1
-    token = TestAuth(u=user, p=pswd, test_count=test_count)
-    test_count += 1
-    test_count = RunTest(TestAccount,token,test_count)
-    test_count = RunTest(TestVoiceApps,token,test_count)
+    TEST_COUNT = 1
+    token = test_auth(user_name=user, password=pswd, count=TEST_COUNT)
+    TEST_COUNT += 1
+    run_test(test_account,token,TEST_COUNT)
+    run_test(test_voice_app,token,TEST_COUNT)
 
-    print(f'{test_count - 1} tests passed')
+    print(f'{TEST_COUNT - 1} tests passed')
