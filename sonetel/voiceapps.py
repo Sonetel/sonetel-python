20a5273246695fc076a3a9a220d8e78cc03e67dd
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2023-04-26 17:37:04 +0530

    cleanup

--- sonetel/voiceapps.py
+++ sonetel/voiceapps.py
@@ -7,7 +7,7 @@ from . import exceptions as e
 
 class VoiceApp(util.Resource):
     """
-    Voice app calss.
+    Voice app class.
     """
     def __init__(self, access_token):
         if not access_token:
@@ -16,29 +16,16 @@ class VoiceApp(util.Resource):
         super().__init__(access_token=access_token)
         self._url = f'{const.API_URI_BASE}{const.API_ENDPOINT_ACCOUNT}{self._accountid}{const.API_ENDPOINT_VOICEAPP}'
 
-    def get(self):
+    def get(self, app_id: str = None):
         """
-        Get voice apps
+        Get voice apps. Specify the app_id to get a specific voice app or leave it blank to get all voice apps in the Sonetel account.
+
+        :param app_id: Optional. The ID of the voice app to get. Defaults to None.
         """
+        if app_id:
+            self._url = f'{self._url}/{app_id}'
+        
         return util.send_api_request(
             token=self._token,
             uri=self._url
         )
-
-    def add(self, voice_app_name: str, voice_app_type: str):
-        """
-        add a new voice app
-        """
-        raise NotImplementedError
-
-    def update(self, voice_app_id: str):
-        """
-        Update a voice app
-        """
-        raise NotImplementedError
-
-    def delete(self, voice_app_id: str):
-        """
-        delete existing voice app
-        """
-        raise NotImplementedError
