5c9c712c8a7cb5627c60433a3aa73574c3b6ee6d
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2023-12-08 11:47:42 +0530

    delete voice app method

--- sonetel/voiceapps.py
+++ sonetel/voiceapps.py
@@ -28,3 +28,13 @@ class VoiceApp(util.Resource):
             self._url = f"{self._url}/{app_id}"
 
         return util.send_api_request(token=self._token, uri=self._url)
+
+    def delete(self, app_id: str):
+        """
+        Delete a voice app.
+
+        :param app_id: The ID of the voice app to delete.
+        """
+        self._url = f"{self._url}/{app_id}"
+
+        return util.send_api_request(token=self._token, uri=self._url, method="DELETE")
